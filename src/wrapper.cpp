f6b0ca4254510c810eb657ec3049272deee6517b
kegel <kegel@kth.se>
2026-05-12 21:45:06 +0200

    add overload step

--- src/wrapper.cpp
+++ src/wrapper.cpp
@@ -18,7 +18,7 @@ void declare_isvd(py::module &m, const std::string &typestr) {
   py::class_<Class>(m, pyclass_name.c_str())
       .def(py::init<int, scalar>(), py::arg("r"), py::arg("ff") = 1.0)
       .def("fit", &Class::fit)
-      .def("increment", py::overload_cast<EigVector> (& Class::increment))
+      .def("increment", py::overload_cast<EigVector>(&Class::increment))
       .def("increment", py::overload_cast<EigMatrix>(&Class::increment))
       .def_readonly("U", &Class::U)
       .def_readonly("S", &Class::S);
